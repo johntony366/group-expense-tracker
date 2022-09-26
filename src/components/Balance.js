bca44fde4b17d1ab3a635b35617573e77d59b100
John <johntony366@gmail.com>
2022-09-26 17:22:21 +0530

    Wire up balance with global state

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Box, Typography } from "@mui/material"
 
+import { useTransactions } from '../context/TransactionProvider'
+
 export const Balance = () => {
+  const transactions = useTransactions();
+  const balance = transactions.reduce((sum, transaction) => sum + Number(transaction.amount), 0)
+  
   return (
     <Box className="balance" sx={{
       width: "100%",
       my: "24px"
     }}>
       <Typography variant="h4">Your Balance:</Typography>
-      <Typography variant="h3">$250</Typography>
+      <Typography variant="h3">{balance < 0 && "-"}${Math.abs(balance)}</Typography>
     </Box>
   )
 }
